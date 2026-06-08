ca67848720929e56598039cb82fc6e5f65f6b623
DHuyenTrang <huyentrang111120044@gmail.com>
2026-06-08 21:57:02 +0700

    feat: update docker-compose and init-dbs.sql for service URL consistency and database creation checks

--- init-dbs.sql
+++ init-dbs.sql
@@ -1,12 +1,21 @@
-CREATE DATABASE gateway_db;
-CREATE DATABASE user_db;
-CREATE DATABASE staff_db;
-CREATE DATABASE product_db;
-CREATE DATABASE cart_db;
-CREATE DATABASE comment_db;
-CREATE DATABASE order_db;
-CREATE DATABASE payment_db;
-CREATE DATABASE shipping_db;
+SELECT 'CREATE DATABASE gateway_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'gateway_db')\gexec
+SELECT 'CREATE DATABASE user_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'user_db')\gexec
+SELECT 'CREATE DATABASE staff_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'staff_db')\gexec
+SELECT 'CREATE DATABASE product_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'product_db')\gexec
+SELECT 'CREATE DATABASE cart_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cart_db')\gexec
+SELECT 'CREATE DATABASE comment_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'comment_db')\gexec
+SELECT 'CREATE DATABASE order_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'order_db')\gexec
+SELECT 'CREATE DATABASE payment_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'payment_db')\gexec
+SELECT 'CREATE DATABASE shipping_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shipping_db')\gexec
 
 GRANT ALL PRIVILEGES ON DATABASE gateway_db TO root;
 GRANT ALL PRIVILEGES ON DATABASE user_db TO root;
